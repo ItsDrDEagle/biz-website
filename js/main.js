aff2ce5fc2449d56df6a8e7a6e02d37f6ef4c4ec
Dorian Kovačević <doriankovacevic7189@gmail.com>
2020-12-11 15:15:57 +0100

    some formatting changes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
-$(document).ready(function(){
-    $("#naslovnaBtn").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#header").offset().top},
-            'slow');
-    });
-    $("#uslugeBtn").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#usluge").offset().top},
-            'slow');
-    });
-    $("#projektiBtn").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#second").offset().top},
-            'slow');
-    });
-    $("#oNamaBtn").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#o-nama").offset().top},
-            'slow');
-    });
-    $("#kontaktBtn").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#kontakt").offset().top},
-            'slow');
-    });
-
-
+$(document).ready(function () {
+  $("#naslovnaBtn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#header").offset().top,
+      },
+      "slow"
+    );
+  });
+  $("#uslugeBtn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#usluge").offset().top,
+      },
+      "slow"
+    );
+  });
+  $("#projektiBtn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#second").offset().top,
+      },
+      "slow"
+    );
+  });
+  $("#oNamaBtn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#o-nama").offset().top,
+      },
+      "slow"
+    );
+  });
+  $("#kontaktBtn").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: $("#kontakt").offset().top,
+      },
+      "slow"
+    );
+  });
 });
